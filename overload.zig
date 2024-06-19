f92f6aad40d66f1e26d48cf75a12ee2a4f290aa2
fig <86081585+fig-eater@users.noreply.github.com>
2024-06-19 05:31:58 -0500

    function overloader completed

--- overload.zig
+++ overload.zig
@@ -1,73 +1,4 @@
-const std = @import("std");
-const heap = std.heap;
-const generator = @import("generator.zig");
-const Allocator = std.mem.Allocator;
-const AnyReader = std.io.AnyReader;
-
-pub fn main() !void {
-    // var gpa = heap.GeneralPurposeAllocator(.{}){};
-    // defer {
-    //     if (gpa.deinit() == .leak) {
-    //         std.debug.print("Memory leaked\n", .{});
-    //     }
-    // }
-    // const allocator = gpa.allocator();
-    // const args = try std.process.argsAlloc(allocator);
-    // defer std.process.argsFree(allocator, args);
-    // if (args.len < 3) {
-    //     std.debug.print("usage: {s} EXTENSION_API_PATH OUTPUT_PATH\n", .{args[0]});
-    //     return;
-    // }
-    // const input_file = try std.fs.cwd().openFile(args[1], .{});
-    // defer input_file.close();
-
-    // try generator.generate(allocator, input_file.reader().any(), args[2]);
-    const v = Vector2{ .x = 2.0, .y = 2.0 };
-    const r = Rect2{ .position = v, .size = v };
-    const vi = Vector2i{ .x = 2, .y = 2 };
-    const ri = Rect2i{ .position = vi, .size = vi };
-    // init(.{2});
-    init({});
-    init(r);
-    init(ri);
-    init(.{ v, v });
-    init(.{ 4.0, 4.0, 4.0, 4.0 });
-    init(.{ Vector2{ .x = 2.0, .y = 3.0 }, Vector2{ .x = 2.0, .y = 3.0 } });
-}
-
-const Vector2 = struct { x: f32, y: f32 };
-const Vector2i = struct { x: i32, y: i32 };
-const Rect2 = struct { position: Vector2, size: Vector2 };
-const Rect2i = struct { position: Vector2i, size: Vector2i };
-
-fn init0() void {
-    std.debug.print("0\n", .{});
-}
-
-fn init1(a: Rect2) void {
-    std.debug.print("1 {any}\n", .{a});
-}
-
-fn init2(a: Rect2i) void {
-    std.debug.print("2 {any}\n", .{a});
-}
-
-fn init3(a: Vector2, b: Vector2) void {
-    std.debug.print("3 {any} {any}\n", .{ a, b });
-}
-
-fn init4(a: f32, b: f32, c: f32, d: f32) void {
-    std.debug.print("4 {d} {d} {d} {d}\n", .{ a, b, c, d });
-}
-const init = makeOverloaded(.{
-    init0, // void
-    init1, // from: Rect2
-    init2, // from: Rect2i
-    init3, // postition: Vector2, size: Vector2
-    init4, // x: f32, y: f32, w: f32, h: f32
-});
-
-fn makeOverloaded(comptime functions: anytype) fn (args: anytype) OverloadedFnReturnType(functions) {
+pub fn makeOverloaded(comptime functions: anytype) fn (args: anytype) OverloadedFnReturnType(functions) {
     comptime {
         const ReturnType = OverloadedFnReturnType(functions);
 
@@ -119,7 +50,7 @@ fn makeOverloaded(comptime functions: anytype) fn (args: anytype) OverloadedFnRe
         const MatchingFunctionType = enum { none, single, multiple };
 
         return struct {
-            fn overloaded(args: anytype) ReturnType {
+            fn overloadedfn(args: anytype) ReturnType {
                 const func, const matching_type: MatchingFunctionType = comptime is_single_block: {
                     const ArgsType = @TypeOf(args);
                     const args_ti = @typeInfo(ArgsType);
@@ -162,11 +93,11 @@ fn makeOverloaded(comptime functions: anytype) fn (args: anytype) OverloadedFnRe
                     .multiple => return @call(.auto, func, args),
                 }
             }
-        }.overloaded;
+        }.overloadedfn;
     }
 }
 
-fn hasSameArgs(comptime a: anytype, comptime b: anytype) bool {
+pub fn hasSameArgs(comptime a: anytype, comptime b: anytype) bool {
     if (a == b) return true;
     const ati = @typeInfo(a);
     const bti = @typeInfo(b);
@@ -180,7 +111,7 @@ fn hasSameArgs(comptime a: anytype, comptime b: anytype) bool {
     return true;
 }
 
-fn OverloadedFnReturnType(comptime functions: anytype) type {
+pub fn OverloadedFnReturnType(comptime functions: anytype) type {
     comptime switch (@typeInfo(@TypeOf(functions))) {
         .Struct => |s| {
             if (s.fields.len <= 0) return noreturn;
@@ -214,56 +145,4 @@ pub fn isConvertableTo(comptime From: type, comptime To: type) bool {
             else => false,
         };
     }
-}
-
-fn makeOverloaded2(comptime ReturnType: type, comptime overload_args: anytype) fn (args: anytype) ReturnType {
-    for (overload_args[1]) |tfp| {
-        if (@typeInfo(tfp[0]) != .Struct) {
-            @compileError("expected struct found " ++ @typeName(tfp[0]));
-        }
-    }
-
-    return struct {
-        fn f(args: anytype) ReturnType {
-            const ArgsType = @TypeOf(args);
-            const args_type_info = @typeInfo(ArgsType);
-
-            const type_fn_pair, const is_single: bool = comptime type_fn_pair: {
-                for (overload_args[0]) |overload_tfp| {
-                    if (ArgsType == overload_tfp[0]) {
-                        break :type_fn_pair .{ overload_tfp, true };
-                    }
-                }
-
-                switch (args_type_info) {
-                    .Struct => |args_struct_info| {
-                        multiple_loop: for (overload_args[1]) |overload_tfp| {
-                            const struct_info = @typeInfo(overload_tfp[0]).Struct;
-                            if (ArgsType == overload_tfp[0]) {
-                                break :type_fn_pair .{ overload_tfp, false };
-                            } else if (args_struct_info.fields.len == struct_info.fields.len) {
-                                for (args_struct_info.fields, struct_info.fields) |a, b| {
-                                    if (!isConvertableTo(a.type, b.type)) {
-                                        continue :multiple_loop;
-                                    }
-                                }
-                                break :type_fn_pair .{ overload_tfp, false };
-                            }
-                        }
-                    },
-                    else => {},
-                }
-
-                @compileError("");
-            };
-
-            const call_args = comptime switch (args_type_info) {
-                .Void => .{},
-                .Struct => if (is_single) .{args} else args,
-                else => .{args},
-            };
-
-            return @call(.auto, type_fn_pair[1], call_args);
-        }
-    }.f;
 }
