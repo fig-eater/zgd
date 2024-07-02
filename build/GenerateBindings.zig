15554e12c9f14d58196388e429ae3e9efd7bb00e
fig <86081585+fig-eater@users.noreply.github.com>
2024-07-02 17:47:06 -0500

    update

---
+++ build/GenerateBindings.zig
@@ -0,0 +1,47 @@
+const Self = @This();
+const std = @import("std");
+const Step = std.Build.Step;
+
+pub const base_id: Step.Id = .custom;
+
+step: Step,
+output_root: std.Build.GeneratedFile,
+
+pub const Options = struct {
+    godot_path: ?std.Build.LazyPath = null,
+};
+
+pub fn create(owner: *std.Build, options: Options) *Self {
+    const api_version = owner.allocator.create(Self) catch @panic("OOM");
+    const step = Step.init(.{
+        .id = base_id,
+        .name = "gdextension api version",
+        .owner = owner,
+        .makeFn = make,
+    });
+    api_version.* = Self{
+        .step = step,
+        .godot_path = options.godot_path,
+        .version_file = .{ .step = step },
+    };
+
+    return api_version;
+}
+
+fn make(step: *Step, _: std.Progress.Node) !void {
+    const b = step.owner;
+
+    const dump_api: *Self = @fieldParentPtr("step", step);
+
+    const file = try b.build_root.handle.createFile("api-dump/" ++ "version", .{});
+    defer file.close();
+    const writer = file.writer();
+
+    var out_code: u8 = 0;
+    const std_out = try b.runAllowFail(&.{ dump_api.godot_path, "--version" }, &out_code, .Ignore);
+    defer b.allocator.free(std_out);
+    var seq = std.mem.splitSequence(u8, std_out, "\n");
+    try writer.print("{s}\n{s}\n", .{ seq.first(), @import("builtin").zig_version_string });
+
+    dump_api.version_file.getPath();
+}
